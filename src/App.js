b705242b8c572d6f7b48bf708674a49d65d864c0
Lipão <filipe.cunha@gmail.com>
2021-04-27 21:43:20 -0300

    Change text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>!@#$</h1>
+      <h1>FEATURE!!!!</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
